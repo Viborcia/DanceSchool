3c9b5cd3d2eab89fb4a1aff810a88910198faef6
Viborcia <susuniak@gmail.com>
2024-10-25 02:17:13 +0200

    authenticate_admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,13 @@
 class AdminController < ApplicationController
+    before_action :authenticate_admin!
+    def admin
+        unless current_user.admin?
+            flash[:error] = 'Brak dostępu!'
+            redirect_to root_path
+          end
+          @users = User.all
+    end
+    def authenticate_admin!
+      redirect_to root_path, alert: 'Brak dostępu!' unless current_user && current_user.admin?
+    end
 end
